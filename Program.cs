4b1327dd93123d68c77f5ef1ed9b07947de9c73c
FabianoEtec <nbscbs@gmail.com>
2021-02-26 21:13:10 -0300

    Finaliza o programa

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using  System ;
+
+namespace  aula_26_02_2021
+{
+     programa de aula
+    {
+        static  void  Main ( string [] args )
+        {
+            string  nomeDoAluno ;
+
+            Console . Limpar ();
+            Console . Beep ();
+
+            Console . Write ( " Olá, aluno. Por favor digite seu nome: " );
+            nomeDoAluno  =  Console . ReadLine ();
+
+            Console . ForegroundColor  =  ConsoleColor . Vermelho ;
+            Console . WriteLine ( " Lista de alunos malvados " );
+            Console . WriteLine ( " ------------------------ " );
+            Console . ForegroundColor  =  ConsoleColor . Verde ;
+            Console . WriteLine ();
+
+            Console . WriteLine ( " 1. Zezinho " );
+            Console . WriteLine ( " 2. Mariazinha " );
+            Console . WriteLine ( " 3. Pedrinho " );
+            Console . WriteLine ( " 4. Jorginho " );
+            Console . WriteLine ( $ " 5. { nomeDoAluno } " );
+
+            Console . WriteLine ();
+            Console . ForegroundColor  =  ConsoleColor . Vermelho ;
+            Console . WriteLine ( " Vão todos para a diretoria! " );
+            Console . Beep ();
+            Console . ResetColor ();
+
+            Console . Write ( " Pressione uma tecla para continuar ... " );
+            Console . ReadKey ();
+        }
+    }
+}
