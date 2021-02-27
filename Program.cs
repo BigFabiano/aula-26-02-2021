b48afde472de47604ffc46413e2a326860b45d07
FabianoEtec <nbscbs@gmail.com>
2021-02-26 21:50:20 -0300

    Correção using System;Aula de 26 Fevereiro de 2021

--- Program.cs
+++ Program.cs
@@ -1,39 +1,39 @@
-﻿using  System ;
+﻿using System;
 
-namespace  aula_26_02_2021
+namespace aula_26_02_2021
 {
-     programa de aula
+    class Program
     {
-        static  void  Main ( string [] args )
+        static void Main(string[] args)
         {
-            string  nomeDoAluno ;
+            string nomeDoAluno;
 
-            Console . Limpar ();
-            Console . Beep ();
+            Console.Clear();
+            Console.Beep();
 
-            Console . Write ( " Olá, aluno. Por favor digite seu nome: " );
-            nomeDoAluno  =  Console . ReadLine ();
+            Console.Write("Olá, aluno. Por favor digite seu nome: ");
+            nomeDoAluno = Console.ReadLine();
 
-            Console . ForegroundColor  =  ConsoleColor . Vermelho ;
-            Console . WriteLine ( " Lista de alunos malvados " );
-            Console . WriteLine ( " ------------------------ " );
-            Console . ForegroundColor  =  ConsoleColor . Verde ;
-            Console . WriteLine ();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Lista de alunos malvados");
+            Console.WriteLine("------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
 
-            Console . WriteLine ( " 1. Zezinho " );
-            Console . WriteLine ( " 2. Mariazinha " );
-            Console . WriteLine ( " 3. Pedrinho " );
-            Console . WriteLine ( " 4. Jorginho " );
-            Console . WriteLine ( $ " 5. { nomeDoAluno } " );
+            Console.WriteLine("1. Zezinho");
+            Console.WriteLine("2. Mariazinha");
+            Console.WriteLine("3. Pedrinho");
+            Console.WriteLine("4. Jorginho");
+            Console.WriteLine($"5. {nomeDoAluno}");
 
-            Console . WriteLine ();
-            Console . ForegroundColor  =  ConsoleColor . Vermelho ;
-            Console . WriteLine ( " Vão todos para a diretoria! " );
-            Console . Beep ();
-            Console . ResetColor ();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Vão todos para a diretoria!");
+            Console.Beep();
+            Console.ResetColor();
 
-            Console . Write ( " Pressione uma tecla para continuar ... " );
-            Console . ReadKey ();
+            Console.Write("Pressione uma tecla para continuar...");
+            Console.ReadKey();
         }
     }
 }
